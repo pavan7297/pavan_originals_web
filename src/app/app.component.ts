0bd1cd5584ef4d4881ec820d7a018f9c940fb9b7
pavanoriginals <pavanoriginal54@gmail.com>
2023-11-26 23:50:05 +0530

    social medial update in screen 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// import { PrimeIcons } from 'primeng/api';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule,],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  weclome="Hello, It's Me";
-  name = "pavan";
-  means="And I'a";
-  description="I'm a webdeveloper with experience over 2 years.";
-  description2="experties is to create a website design, Frontend design and backend design";
-  description3="many more....."
+  weclome = "Hello, It's Me";
+  name = 'pavan srinivas';
+  means = "And I'a";
+  description = "I'm a webdeveloper with experience over 2 years.";
+  description2 =
+    'experties is to create a website design, Frontend design and backend design';
+  description3 = 'many more.....';
 }
